ff13fc5adc030a35b329a93f7d98553a09c537b5
andrewhxd <andrewhxue@gmail.com>
2025-01-25 00:44:10 -0500

    Initial Commit

---
+++ PosePal/PosePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PosePalApp.swift
+//  PosePal
+//
+//  Created by Andrew Xue on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PosePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
