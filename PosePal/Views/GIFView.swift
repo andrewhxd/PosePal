48ac79a722ac81eb3883ae92f11aa63ecf7aad61
andrewhxd <andrewhxue@gmail.com>
2025-01-26 12:58:53 -0500

    loading

---
+++ PosePal/Views/GIFView.swift
@@ -0,0 +1,8 @@
+//
+//  GIFView.swift
+//  PosePal
+//
+//  Created by Andrew Xue on 1/26/25.
+//
+
+import Foundation
---
+++ PosePal/Views/Models/Tip.swift
@@ -0,0 +1,8 @@
+//
+//  Tip.swift
+//  PosePal
+//
+//  Created by Andrew Xue on 1/26/25.
+//
+
+import Foundation
